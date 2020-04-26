27fff56dfbff6b46cebbfaac2aac2861dcd66906
SunKrish <64076031+SunKrish@users.noreply.github.com>
2020-04-26 22:32:30 +0530

    Add files via upload

---
+++ PythonNotebookforGit.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook is used for Python Git Project
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
